2ae4633d824426bdf6f684f091e7c51220134cf4
Matt Gale <mgale694@gmail.com>
2023-01-14 19:52:06 +0000

    Initial commit

---
+++ create.py
@@ -0,0 +1,17 @@
+import sys
+import os
+from github import Github
+
+path = os.getenv("FILEPATH")
+username = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
+
+def create():
+    folderName = str(sys.argv[1])
+    os.makedirs(path + str(folderName))
+    user = Github(username, password).get_user()
+    repo = user.create_repo(folderName)
+    print(f"Succesfully created repository {folderName}")
+
+if __name__ == "__main__":
+    create()
